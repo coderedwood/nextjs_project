4475a16b9036d6cba768be16ce1e6d574c365850
Dwayne Redwood <87104481+coderedwood@users.noreply.github.com>
2022-11-29 23:24:19 -0500

    added some designs to navbar

--- metaverse-demo/components/Navbar.jsx
+++ metaverse-demo/components/Navbar.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
@@ -9,9 +10,27 @@ const Navbar = () => (
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
-
   >
-    navbar
+    <div className="absolute w-[50%] inset-0
+    gradient-01"
+    />
+    <div className={`${styles.innerWidth} mx-auto
+    flex justify-between gap-8`}
+    />
+    <img
+      src="/search.svg"
+      alt="search"
+      className="w-[24px] h-[24px] object-contain"
+    />
+    <h2 className="font-extrabold text-[24px]
+    leading-[30px] text-white"
+    >
+      METAVERSUS
+    </h2>
+    <img src="/menu.svg"
+      alt="menu"
+      className="w-[24px] h-[24px] object-contain"
+    />
   </motion.nav>
 );
 
